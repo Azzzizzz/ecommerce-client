7c64eef83745ec731f1f576fb284f0bca3d14aa7
syed.abdul.aziz <syed.abdul.aziz@topica.edu.vn>
2022-06-19 00:33:45 +0530

    mui 4.x integeration and mui ssr styles fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,43 @@
 import "../styles/globals.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
 const client = new ApolloClient({
   uri: "http://localhost:1337/graphql",
   cache: new InMemoryCache(),
 });
-function MyApp({ Component, pageProps }) {
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />;
-    </ApolloProvider>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <ApolloProvider client={client}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
+
+  // return (
+  //   <ApolloProvider client={client}>
+  //     <Component {...pageProps} />;
+  //   </ApolloProvider>
+  // );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
+import { Button } from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const GET_PRODUCTS = gql`
   query Query {
@@ -22,14 +24,37 @@ const GET_PRODUCTS = gql`
   }
 `;
 
+const useStyles = makeStyles((theme) => ({
+  heading1: {
+    color: "red",
+  },
+}));
+
 const Home = () => {
   const { data, loading, error } = useQuery(GET_PRODUCTS);
+  const classes = useStyles();
 
   if (loading) <h1>loading..</h1>;
-  if (error) console.log(error, "somthing went wrong");
+  if (error) console.log(error, "something went wrong");
   if (data) console.log(data);
 
-  return <div>E-Commerce</div>;
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+      <h1 className={classes.heading1}>Hello World</h1>
+    </div>
+  );
 };
 export default Home;
 
