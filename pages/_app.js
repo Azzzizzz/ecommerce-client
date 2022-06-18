f0fb739cbda4b4d96a8fccecf8da0aa73622fcd3
syed.abdul.aziz <syed.abdul.aziz@topica.edu.vn>
2022-06-18 22:32:11 +0530

    inital setup and graphql apllo integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
